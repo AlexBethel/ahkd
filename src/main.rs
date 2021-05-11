638b069983f6f060518b0122831c76fdfdbd9da1
Alexander Bethel <zanbethel1@gmail.com>
2021-05-11 10:44:18 -0500

    Fix copyright headers
    Probably a good idea to explicitly say that the source files are
    licenced under GPL, not just that the program is licensed under GPL.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 // Main program entry point.
 // Copyright (C) 2021 by Alexander Bethel.
 
+// This file is part of ahkd.
+
 // ahkd is free software: you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
